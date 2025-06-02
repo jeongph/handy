7d8c4f358be76a3af46d0659102ebca7fa9402e2
JeongUk Park <dnr1105@gmail.com>
2025-06-02 21:05:59 +0900

    Create BaseTimeEntity.java

---
+++ spring/BaseTimeEntity.java
@@ -0,0 +1,14 @@
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
+public class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+}
